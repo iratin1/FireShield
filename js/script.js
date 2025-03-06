e8e539a2fea240d230ba366947228ddfe799227c
Caio Rodgheri <156906055+iratin1@users.noreply.github.com>
2025-03-06 14:27:16 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,66 +38,6 @@ document.addEventListener("DOMContentLoaded", function () {
     fadeInOnScroll(); // Chama a função uma vez ao carregar a página
 });
 
-
-
-window.onload = () => {
-    setTimeout(() => {
-        window.scrollTo(0, 0); // Garante que a página seja rolada para o topo
-      }, 10);
-    const scrollDownBtn = document.getElementById('scrollDownBtn');
-    const scrollUpBtn = document.getElementById('scrollUpBtn');
-    
-    
-    // Função que verifica a posição da rolagem
-    function checkScrollPosition() {
-      if (window.scrollY === 0) {
-        scrollDownBtn.classList.add('show'); // Mostra o botão de "Role para baixo"
-        scrollUpBtn.classList.remove('show'); // Esconde o botão de "Voltar para cima"
-      } else {
-        scrollDownBtn.classList.remove('show'); // Esconde o botão de "Role para baixo"
-        scrollUpBtn.classList.add('show'); // Mostra o botão de "Voltar para cima"
-      }
-    }
-    
-    // Chama a função ao carregar a página e sempre que a rolagem mudar
-    window.addEventListener('scroll', checkScrollPosition);
-    checkScrollPosition();
-    
-    // Função para rolar suavemente
-    function smoothScrollTo(targetPosition) {
-      const startPosition = window.scrollY;
-      const distance = targetPosition - startPosition;
-      const duration = 1000; // Duração da animação em milissegundos
-      let startTime = null;
-    
-      function scrollAnimation(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        const timeElapsed = currentTime - startTime;
-        const run = easeInOut(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, run);
-        if (timeElapsed < duration) {
-          requestAnimationFrame(scrollAnimation);
-        }
-      }
-    
-      function easeInOut(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return (c / 2) * t * t + b;
-        t--;
-        return (-c / 2) * (t * (t - 2) - 1) + b;
-      }
-    
-      requestAnimationFrame(scrollAnimation);
-    }
-     
-    // Ação para o botão "Voltar para cima"
-    scrollUpBtn.onclick = () => {
-      smoothScrollTo(0); // Rola suavemente para o topo
-    };
-  };
-  
-
-
 // Obtendo elementos
 const features = document.querySelectorAll('.feature'); // Todas as features
 const popup = document.getElementById('popup');
